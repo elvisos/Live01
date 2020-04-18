7f3a45890f34d3d397a25114ed324bbf17f7b6da
elvisos <acolitoelvis@gmail.com>
2020-04-18 15:06:39 -0300

    Estrututa e adição do corpo da página index.html e criação do araquivo script.js com o acionamento do botão

---
+++ script.js
@@ -0,0 +1,8 @@
+var cx = document.getElementById("caixa");
+var sd = document.getElementById("saida");
+var bt = document.getElementById("btn");
+
+bt.onclick = function(){
+    cx.innerHTML = "<h1>foi adicionada uma imagem a tela</h1>";
+    
+};
